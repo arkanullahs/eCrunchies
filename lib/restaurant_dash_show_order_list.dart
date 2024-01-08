86167b8b9aa9b3ec6c09bff86ccd6fdd0f978182
Arkanullah Saad <120630818+arkanullahs@users.noreply.github.com>
2024-01-08 14:56:30 +0600

    A complete new User Interface for restaurant_dash.dart

---
+++ lib/restaurant_dash_show_order_list.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class ShowOrder {
+  Widget buildOrderList(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Order List'),
+      ),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance
+            .collection('orders')
+            .orderBy('timeSent', descending: true)
+            .snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          }
+
+          final orderData = snapshot.data!.docs;
+
+          if (orderData.isEmpty) {
+            return Center(child: Text('No orders available.'));
+          }
+
+          return ListView.builder(
+            itemCount: orderData.length,
+            itemBuilder: (context, index) {
+              final foodName = orderData[index]['foodName'];
+              final quantity = orderData[index]['quantity'];
+              final price = orderData[index]['price'];
+              final timeSent =
+              (orderData[index]['timeSent'] as Timestamp).toDate();
+
+              return ListTile(
+                title: Text('Food: $foodName'),
+                subtitle: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Quantity: $quantity'),
+                    Text('Price: $price'),
+                    Text('Time Sent: ${_formatDateTime(timeSent)}'),
+                  ],
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+
+  String _formatDateTime(DateTime dateTime) {
+    return '${dateTime.day}/${dateTime.month}/${dateTime.year} ${dateTime.hour}:${dateTime.minute}';
+  }
+}
