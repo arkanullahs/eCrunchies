7899ec9a832957cbbb6a76ef718d892f540bb952
Khadiza Khanom <lk221201@gmail.com>
2024-01-14 02:19:36 +0600

    updated search button.

--- lib/order_screen.dart
+++ lib/order_screen.dart
@@ -1,10 +1,20 @@
+
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'food_details_screen.dart';
 import 'food_items_screen.dart';
 import 'discover_screen.dart';
 import 'foods_screen.dart';
+import 'restaurant_menu_screen.dart';
 
-class OrderScreen extends StatelessWidget {
+class OrderScreen extends StatefulWidget {
+  @override
+  _OrderScreenState createState() => _OrderScreenState();
+}
+
+class _OrderScreenState extends State<OrderScreen> {
+  String searchQuery = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,8 +23,10 @@ class OrderScreen extends StatelessWidget {
         actions: [
           GestureDetector(
             onTap: () {
-              // Implement search functionality
-              print('Search button tapped!');
+              showSearch(
+                context: context,
+                delegate: RestaurantSearchDelegate(), // Use custom search delegate
+              );
             },
             child: Padding(
               padding: const EdgeInsets.symmetric(horizontal: 16.0),
@@ -56,18 +68,122 @@ class OrderScreen extends StatelessWidget {
               ),
             ),
             FoodItemsScreen(),
-            //Padding(
-              //padding: const EdgeInsets.all(16.0),
-              //child: Text(
-                //'Discover',
-               // style: TextStyle(
-                 // fontWeight: FontWeight.bold,
-                 // fontSize: 18,
-                //),
-              //),
-            //),
             DiscoverScreen(),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class RestaurantSearchDelegate extends SearchDelegate<String> {
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return [
+      IconButton(
+        icon: Icon(Icons.clear),
+        onPressed: () {
+          query = '';
+        },
+      ),
+    ];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+      icon: Icon(Icons.arrow_back),
+      onPressed: () {
+        close(context, '');
+      },
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    return SearchResults(searchQuery: query);
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    return SearchResults(searchQuery: query);
+  }
+}
+
+class SearchResults extends StatelessWidget {
+  final String searchQuery;
+
+  SearchResults({required this.searchQuery});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+      stream: FirebaseFirestore.instance.collection('restaurants')
+          .where('name', isGreaterThanOrEqualTo: searchQuery)
+          .where('name', isLessThan: searchQuery + 'z')
+          .snapshots(),
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return Center(
+            child: Text('Error: ${snapshot.error}'),
+          );
+        }
+
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+
+        final List<DocumentSnapshot> documents = snapshot.data!.docs;
+
+        return ListView.builder(
+          itemCount: documents.length,
+          itemBuilder: (context, index) {
+            final DocumentSnapshot document = documents[index];
+            final String name = document['name'];
+            final String imageUrl = document['imageUrl'];
+
+            return GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => RestaurantMenuScreen(restaurantName: name),
+                  ),
+                );
+              },
+              child: RestaurantCard(
+                imageUrl: imageUrl,
+                name: name,
+              ),
+            );
+          },
+        );
+      },
+    );
+  }
+}
+
+class RestaurantCard extends StatelessWidget {
+  final String imageUrl;
+  final String name;
+
+  RestaurantCard({required this.imageUrl, required this.name});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: Image.network(
+          imageUrl,
+          width: 50,
+          height: 50,
+          fit: BoxFit.cover,
+        ),
+        title: Text(
+          name,
+          style: TextStyle(fontWeight: FontWeight.bold),
         ),
       ),
     );
